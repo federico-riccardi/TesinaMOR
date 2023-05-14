a0507e93df9f055fb8122aff35dd6b054f91a9a7
debora.bisoglio <deborabisoglio@gmail.com>
2023-05-15 00:14:41 +0200

    Aggiornato toMesh.sh

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-we
-ciao 
-prova per merge
+## toMesh.sh
+
+N.B. Alla riga 18 bisogna sostituire al posto di /root/TesinaMOR il vostro path assoluto della directory (per 4 volte).
+So che è fastidioso, controllate prima di usarlo.
+
+Se dà errore BLAS provare con questo "sudo apt-get install libopenblas-dev".
--- toMesh.sh
+++ toMesh.sh
@@ -8,20 +8,13 @@ cd CppToPython
 git submodule init
 git submodule update
 mkdir -p externals
-cd gedim/3rd_party_libraries
-meshes.py
-python cmake -DINSTALL_VTK=OFF -DINSTALL_LAPACK=OFF ../gedim/3rd_party_libraries 
-python make -j4
+cd externals
+cmake -DINSTALL_VTK=OFF -DINSTALL_LAPACK=OFF ../gedim/3rd_party_libraries 
+make -j4
 cd ..
-
-
-
-
-!cmake -DINSTALL_VTK=OFF -DINSTALL_LAPACK=OFF ../gedim/3rd_party_libraries
-!make -j4
-%cd ..
-    "mkdir -p release",
-    "cd release ",
-    "cmake -DCMAKE_PREFIX_PATH= ../ ",
-    "make -j4 GeDiM4Py",
-    "cd .."
+mkdir -p release
+cd release 
+path_1 = 
+cmake -DBLAS_LIBRARIES= -DCMAKE_PREFIX_PATH="/root/TesinaMOR/CppToPython/externals/Main_Install/eigen3;/root/TesinaMOR/CppToPython/externals/Main_Install/triangle;/root/TesinaMOR/CppToPython/externals/Main_Install/tetgen;/root/TesinaMOR/CppToPython/externals/Main_Install/googletest" ../
+make -j4 GeDiM4Py
+cd ..
