cbb6174ec524c99ba1ff1ce1c29b83536e3f1203
federico-riccardi <federico.riccardi999@gmail.com>
2023-05-18 15:01:50 +0200

    definita classe Net, funzione beta e residuo pde

--- PINN.py
+++ PINN.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 
+
+
 import numpy as np
 import sys
 import os
@@ -45,3 +47,49 @@ import warnings
 warnings.filterwarnings('ignore')
 
 np.random.seed(1234)
+
+mu_1_range = [0.1,10.]
+mu_2_range = [-1., 1.]
+P = np.array([mu_1_range,mu_2_range])
+input_dim = 2 + P.shape[0]
+output_dim = 1
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.input_layer = nn.Linear(input_dim,5)
+        self.hidden_layer1 = nn.Linear(5,5)
+        self.hidden_layer2 = nn.Linear(5,5)
+        self.hidden_layer3 = nn.Linear(5,5)
+        self.hidden_layer4 = nn.Linear(5,5)
+        self.output_layer = nn.Linear(5,output_dim)
+
+    def forward(self, x,y,mu_1,mu_2):
+        input = torch.cat([x,y,mu_1,mu_2],axis=1) # combines the column array
+        layer1_out = torch.sigmoid(self.input_layer(input))
+        layer2_out = torch.sigmoid(self.hidden_layer1(layer1_out))
+        layer3_out = torch.sigmoid(self.hidden_layer2(layer2_out))
+        layer4_out = torch.sigmoid(self.hidden_layer3(layer3_out))
+        layer5_out = torch.sigmoid(self.hidden_layer4(layer4_out))
+        output = self.output_layer(layer5_out)
+        return output
+    
+def beta(x,y):
+    return np.array([y*(1-y),0])
+    
+def R(x,y,mu_1,mu_2,net): #residuo pde
+    u = net(x,y,mu_1,mu_2)
+    u_x = torch.autograd.grad(u.sum(), x, create_graph=True)[0]
+    u_xx = torch.autograd.grad(u_x.sum(), x, create_graph=True)[0]
+    u_y = torch.autograd.grad(u.sum(), y, create_graph=True)[0]
+    u_yy = torch.autograd.grad(u_y.sum(), y, create_graph=True)[0]
+    pde = mu_1*(u_xx + u_yy) + beta(x,y)[0]*u_x + beta(x,y)[1]*u_y
+    return pde
+
+
+
+net = Net()
+mse_cost_function = torch.nn.MSELoss() # Mean squared error
+optimizer = torch.optim.Adam(net.parameters())
+
