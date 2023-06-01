2cb75056f3463d7626b78448cc4ac01e732fa05b
federico-riccardi <federico.riccardi999@gmail.com>
2023-06-01 17:36:35 +0200

    GIRA. Completata scrittura greedy

--- Greedy.py
+++ Greedy.py
@@ -71,7 +71,7 @@ def Poisson_weakTerm_down(numPoints, points):
 
 [advection, advectionStrong] = gedim.AssembleAdvectionMatrix(Poisson_b, problemData, lib)
 
-[mass, massStrong] = gedim.AssembleMassMatrix(Poisson_c, problemData, lib) # serve solo per calcolare la costante di Poincaré   q
+[mass, massStrong] = gedim.AssembleReactionMatrix(Poisson_c, problemData, lib) # serve solo per calcolare la costante di Poincaré   q
 
 weakTerm_down = gedim.AssembleWeakTerm(Poisson_weakTerm_down, 2, problemData, lib)
 
@@ -130,13 +130,13 @@ def OfflineResidual(B):
      return [C_11, d_11, d_12, E_11, E_12, E_22]
      
 
-def ErrorEstimate(mu, solN_mu, off_res):
+def ErrorEstimate(mu, solN_mu, off_res, beta_mu):
     pre_mult = [mu[1]**2, -2*mu[0]*mu[1]*solN_mu.T, -2*mu[1]*solN_mu.T, mu[0]**2*solN_mu.T, 2*mu[0]*solN_mu.T, solN_mu.T]
     post_mult = [1., 1., 1., solN_mu, solN_mu, solN_mu]
     error = 0.0
     for i in range(len(off_res)):
         error += pre_mult[i] @ off_res[i] @ post_mult[i]
-    return error
+    return np.sqrt(error)/beta_mu
 
 eigs, vecs = scipy.linalg.eig(stiffness.todense(), mass.todense())
 min_eig = np.min(eigs.real)
@@ -152,7 +152,7 @@ def greedy(N_max, tol):
     training_set_list = training_set.tolist()
     initial_muN = np.random.choice(len(training_set_list) - 1, 1)[0]
     mu_N = training_set_list.pop(initial_muN)
-    beta_mu = InfSupConstant()
+    beta_mu = InfSupConstant(mu_N)
 
     print('Perfom greedy algorithm...')
     while len(training_set_list) > 0 and N < N_max and delta_N > tol:
@@ -162,18 +162,17 @@ def greedy(N_max, tol):
         basis_functions.append(np.copy(basis_function))
         B = np.transpose(np.array(basis_functions))
         BX = np.transpose(B) @ X @ B
-
-        stiffness_RB = 
-        [AQN, fQN] = ProjectSystem(X, weakTerm_down, B) # applica il cambio base
+        stiffness_RB = B.T @ stiffness @ B
+        advection_RB = B.T @ advection @ B
+        weakTerm_down_RB = B.T @ weakTerm_down
         off_res = OfflineResidual(B)
         counter = 0
         mu_selected_index = -1
         max_deltaN = -1.
         for mu in training_set_list:
-            solN_mu = 
-            solN_mu = Solve_reduced_order(AQN, fQN, thetaA(mu), thetaF(mu))
-            deltaN_mu = ErrorEstimate(mu, solN_mu, off_res) / normX(solN_mu, BX)
-	    
+            solN_mu =  np.linalg.solve(mu[0]*stiffness_RB + advection_RB, mu[1]*weakTerm_down_RB)
+            deltaN_mu = ErrorEstimate(mu, solN_mu, off_res, beta_mu) / normX(solN_mu, BX)
+
             if deltaN_mu > max_deltaN:
                 max_deltaN = deltaN_mu
                 mu_selected_index = counter
@@ -183,7 +182,7 @@ def greedy(N_max, tol):
         if mu_selected_index == -1:
             raise Exception('ERROR, parameter not found')
 
-        muN = training_set_list.pop(mu_selected_index)
-        deltaN = max_deltaN
+        mu_N = training_set_list.pop(mu_selected_index)
+        delta_N = max_deltaN
 
     return [N, np.transpose(np.array(basis_functions))]
