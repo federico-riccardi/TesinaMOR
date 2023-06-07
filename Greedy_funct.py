96680a817f91cd2cf70f0ae095f45cc4d14cdd87
debora.bisoglio <deborabisoglio@gmail.com>
2023-06-07 15:48:14 +0200

    runall

--- Greedy_funct.py
+++ Greedy_funct.py
@@ -44,6 +44,7 @@ def Greedy_funct(problemData, lib, M, tol, N_max):
 
     training_set = np.random.uniform(low=P[:, 0], high=P[:, 1], size=(M, P.shape[0]))
 
+
     X = stiffness
     invX = splu(X)
 
@@ -66,7 +67,6 @@ def Greedy_funct(problemData, lib, M, tol, N_max):
         E_22 = B.T @ advection.T @ (invX.solve(advection @ B))
         return [C_11, d_11, d_12, E_11, E_12, E_22]
         
-
     def ErrorEstimate(mu, solN_mu, off_res, beta_mu):
         pre_mult = [np.array([mu[1]**2]), -2*mu[0]*mu[1]*solN_mu.T, -2*mu[1]*solN_mu.T, mu[0]**2*solN_mu.T, 2*mu[0]*solN_mu.T, solN_mu.T]
         post_mult = [np.array([1.]), np.array([1.]), np.array([1.]), solN_mu, solN_mu, solN_mu]
@@ -81,7 +81,6 @@ def Greedy_funct(problemData, lib, M, tol, N_max):
     def InfSupConstant(mu):
         return mu[0]/(1+C_omega**2)
 
-
     N = 0
     basis_functions = []
     B = np.empty((0,0))
@@ -90,9 +89,9 @@ def Greedy_funct(problemData, lib, M, tol, N_max):
     initial_muN = np.random.choice(len(training_set_list) - 1, 1)[0]
     mu_N = training_set_list.pop(initial_muN)
     beta_mu = InfSupConstant(mu_N)
-
     print('Perfom greedy algorithm...')
-    while len(training_set_list) > 0 and N < N_max and delta_N > tol:
+    flag = 0
+    while len(training_set_list) > 0 and N < N_max and delta_N > tol and flag == 0:
         N += 1
         snapshot = gedim.LUSolver(mu_N[0]*X+advection,mu_N[1]*weakTerm_down,lib)
         basis_function = GramSchmidt(B, snapshot)
@@ -107,19 +106,22 @@ def Greedy_funct(problemData, lib, M, tol, N_max):
         mu_selected_index = -1
         max_deltaN = -1.
         for mu in training_set_list:
-            solN_mu =  np.linalg.solve(mu[0]*stiffness_RB + advection_RB, mu[1]*weakTerm_down_RB)
-            deltaN_mu = ErrorEstimate(mu, solN_mu, off_res, beta_mu) / normX(solN_mu, BX)
+            if not np.linalg.det(mu[0]*stiffness_RB + advection_RB) < tol:
+                solN_mu =  np.linalg.solve(mu[0]*stiffness_RB + advection_RB, mu[1]*weakTerm_down_RB)
+                deltaN_mu = ErrorEstimate(mu, solN_mu, off_res, beta_mu) / normX(solN_mu, BX)
 
-            if deltaN_mu > max_deltaN:
-                max_deltaN = deltaN_mu
-                mu_selected_index = counter
+                if deltaN_mu > max_deltaN:
+                    max_deltaN = deltaN_mu
+                    mu_selected_index = counter
 
             counter += 1
 
         if mu_selected_index == -1:
-            raise Exception('ERROR, parameter not found')
+            flag = 1
 
         mu_N = training_set_list.pop(mu_selected_index)
-        delta_N = max_deltaN
+        delta_N = max_deltaN  
+
+    print('We used {} bases'.format(N))
 
     return np.transpose(np.array(basis_functions)), stiffness_RB, advection_RB, weakTerm_down_RB
---
+++ runall_funct.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+#%%
+
+#import stuff
+import argparse
+import numpy as np
+import sys
+import os
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import scipy.io
+from scipy.interpolate import griddata
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.gridspec as gridspec
+import warnings
+import csv
+import time
+
+if not os.path.exists("CppToPython"):
+    sys.exit("CppToPython does not exist, please run mesh.sh.") 
+
+sys.path.append("CppToPython")
+sys.path.insert(0, '../Utilities/')
+
+import GeDiM4Py as gedim
+import PINN_funct
+import Greedy_funct
+
+
+#### Parameters PINN
+meshSize = 0.01
+order = 2
+iterations= 10000
+coeff = [10, 10, 500, 500]
+n_points = 200
+delta = 0.1 #parametro per funzione cutoff
+
+### PARAM GREEDY
+
+M = 200
+N_max = 20
+tol  = 1.e-8
+
+## PARAM SOL
+mu_1 = 1.
+mu_2 = 1.
+
+start_time_PINN_offline = time.time()
+mse_table, net = PINN_funct.PINN_funct(iterations, coeff, n_points, delta)
+end_time_PINN_offline = time.time()
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+x = np.arange(0,1,0.02)
+y = np.arange(0,1,0.02)
+ms_x, ms_y = np.meshgrid(x,y)
+x = np.ravel(ms_x).reshape(-1,1)
+y = np.ravel(ms_y).reshape(-1,1)
+pt_x = Variable(torch.from_numpy(x).float(), requires_grad=True)
+pt_y = Variable(torch.from_numpy(y).float(), requires_grad=True)
+
+start_time_PINN_online = time.time()
+pt_u = net(pt_x, pt_y, mu_1*torch.ones((pt_x.shape[0],1)), mu_2*torch.ones((pt_y.shape[0],1)))
+start_time_PINN_online = time.time()
+#u = pt_u.data.cpu().numpy()
+u = pt_u.numpy(force=True)
+ms_u = u.reshape(ms_x.shape)
+surf = ax.plot_surface(ms_x, ms_y, ms_u, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+fig.savefig('fotoPINN.png')
+
+
+os.chdir("CppToPython")
+lib = gedim.ImportLibrary("./release/GeDiM4Py.so")
+
+config = { 'GeometricTolerance': 1.0e-8 }
+gedim.Initialize(config, lib)
+
+order = 1
+meshSize = 0.001
+
+domain = { 'SquareEdge': 1.0, 'VerticesBoundaryCondition': [1,0,1,1], 'EdgesBoundaryCondition': [2,3,1,1], 'DiscretizationType': 1, 'MeshCellsMaximumArea': meshSize }
+[meshInfo, mesh] = gedim.CreateDomainSquare(domain, lib)
+
+discreteSpace = { 'Order': order, 'Type': 1, 'BoundaryConditionsType': [1, 2, 3, 3] }
+[problemData, dofs, strongs] = gedim.Discretize(discreteSpace, lib)
+
+start_time_Greedy_offline = time.time()
+B, stiffness_RB, advection_RB, weakTerm_down_RB = Greedy_funct.Greedy_funct(problemData, lib, M, tol, N_max)
+end_time_Greedy_offline = time.time()
+
+start_time_Greedy_online = time.time()
+u_RB = np.linalg.solve(mu_1*(stiffness_RB) + (advection_RB), mu_2*(weakTerm_down_RB))
+solution_RB = B @ u_RB
+end_time_Greedy_online = time.time()
+gedim.PlotSolution(mesh, dofs, strongs, solution_RB, np.zeros(problemData['NumberStrongs']), title='Solution_RB')
