adb80d670956e9bc1d0cb1a8b23891551650b9df
debora.bisoglio <deborabisoglio@gmail.com>
2023-06-08 18:47:54 +0200

    plot1

--- PINNvsFEM.py
+++ PINNvsFEM.py
@@ -10,6 +10,8 @@ import PINN_funct
 import numpy as np
 import torch
 from torch.autograd import Variable
+from numpy import linalg as LA
+import csv
 
 if not os.path.exists("CppToPython"):
     sys.exit("CppToPython does not exist, please run mesh.sh.") 
@@ -18,7 +20,7 @@ sys.path.append("CppToPython")
 sys.path.insert(0, '../Utilities/')
 
 import GeDiM4Py as gedim
-
+this_dir = os.getcwd()
 os.chdir("CppToPython")
 lib = gedim.ImportLibrary("./release/GeDiM4Py.so")
 
@@ -37,6 +39,7 @@ print(dofs.shape)
 print(dofs[0])
 
 
+
 ## FEM (posso farlo prima perché è affine)
 stiffness, advection, weakTerm_down = FEM_funct.FEM_funct(problemData, lib)
 pt_x = Variable(torch.from_numpy(np.array([dofs[0]]).T).float(), requires_grad=True)
@@ -48,32 +51,41 @@ pt_y_s = Variable(torch.from_numpy(np.array([strongs[1]]).T).float(), requires_g
 
 ##RUN PINN
 delta = 0.1
-parameters = [[1, 1], [7, 0.7]]
+parameters = [[1, 1], [7, 0.7], [5, 0], [3, -0.5], [0.1, 1]]
 # Open the file and load the file
 with open('/root/TesinaMOR/Configurazioni.yaml') as f:
     data = yaml.load(f, Loader=SafeLoader)
-
-for iter in data['iterations']:
-    for l in data['coeff']:
-        coeff = [float(l[1]), float(l[2]), float(l[3]), float(l[4])]
-        for p in data['points']:
-            mse_table, net = PINN_funct.PINN_funct(iter, coeff, p, delta)
-
-            ## scelgo mu_1 mu_2
-            for combo in parameters:
-                print("combo = {}".format(combo))
-                mu_1 = combo[0]
-                mu_2 = combo[1]
-                solution_FEM = gedim.LUSolver(mu_1*stiffness+advection, mu_2*weakTerm_down, lib)
-                gedim.PlotSolution(mesh, dofs, strongs, solution_FEM, np.zeros(problemData['NumberStrongs']), title='Solution_FEM')
-                solution_PINN = net(pt_x, pt_y, mu_1*torch.ones((pt_x.shape[0],1)), mu_2*torch.ones((pt_y.shape[0],1))).detach().numpy().T[0]
-                solution_PINN_strong = net(pt_x_s, pt_y_s, mu_1*torch.ones((pt_x_s.shape[0],1)), mu_2*torch.ones((pt_y_s.shape[0],1))).detach().numpy().T[0]
+    for iter in data['iterations']:
+        for l in data['coeff']:
+            coeff = [float(l[1]), float(l[2]), float(l[3]), float(l[4])]
+            dir = this_dir+"/results_plot1/{}/{}".format(iter, coeff)
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            with open(dir+'/error.csv', 'w', newline='') as csvfile:
+                fieldnames = ['parameters', 'error_inf', 'error_2']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
                 
+                for p in data['points']:
+                    mse_table, net = PINN_funct.PINN_funct(iter, coeff, p, delta)
+
+                    ## scelgo mu_1 mu_2
+                    for combo in parameters:
+                        print("combo = {}".format(combo))
+                        mu_1 = float(combo[0])
+                        mu_2 = float(combo[1])
+                        solution_FEM = gedim.LUSolver(mu_1*stiffness+advection, mu_2*weakTerm_down, lib)
+                        gedim.PlotSolution(mesh, dofs, strongs, solution_FEM, np.zeros(problemData['NumberStrongs']), title='Solution_FEM')
+                        solution_PINN = net(pt_x, pt_y, mu_1*torch.ones((pt_x.shape[0],1)), mu_2*torch.ones((pt_y.shape[0],1))).detach().numpy().T[0]
+                        solution_PINN_strong = net(pt_x_s, pt_y_s, mu_1*torch.ones((pt_x_s.shape[0],1)), mu_2*torch.ones((pt_y_s.shape[0],1))).detach().numpy().T[0]
+
+                        ##calcolo errore L^inf
+                        int_error = solution_FEM - solution_PINN
+                        error = np.concatenate([int_error, solution_PINN_strong])
+                        l_inf = LA.norm(error, np.inf)
+                        l_2 = LA.norm(error, 2)
+                        writer.writerow({"parameters": [mu_1, mu_2], "error_inf": l_inf, "error_2": l_2})
 
 
-
-
-
-            
 print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("\n")
---
+++ plot1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+import os
+
+app = Dash(__name__)
+
+fig_inf = go.Figure(layout = go.Layout(title = "L_inf error PINN vs FEM", xaxis = {'title':'parameters'}, yaxis = {'title':'error_inf'}, showlegend=True))
+fig_2 = go.Figure(layout = go.Layout(title = "L_2 error PINN vs FEM", xaxis = {'title':'parameters'}, yaxis = {'title':'error_2'}, showlegend=True))
+
+for iterations in os.listdir("results_plot1"):
+    for coeff in os.listdir("results_plot1/"+iterations):
+        df = pd.read_csv("results_plot1/{}/{}/error.csv".format(iterations, coeff))
+        fig_inf.add_trace(go.Scatter(x = df['parameters'], y = df['error_inf'], name="{}/{}".format(iterations, coeff)))
+        fig_2.add_trace(go.Scatter(x = df['parameters'], y = df['error_2'], name="{}/{}".format(iterations, coeff)))
+
+app.layout = html.Div(children=[
+    html.H1(children='Title 1'),
+        html.Div(children='''
+        Subtitle.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig_inf
+    ),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig_2
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+    
